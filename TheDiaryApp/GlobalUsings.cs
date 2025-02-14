d17e1656fa82b2d42c6c6777c76b22924b06910e
Marci <Marci@DESKTOP-446T88O>
2025-02-14 21:07:35 +0500

    Глобальныое исправление вылетов

---
+++ TheDiaryApp/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Fonts;
+global using TheDiaryApp.Repositories;
+global using TheDiaryApp.Models;
+global using TheDiaryApp.Helpers;
+global using TheDiaryApp.ViewModels;
+global using Microsoft.Maui.Networking;
---
+++ TheDiaryApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TheDiaryApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var reportRepo = new ReportRepo(new ExcelParser(), new ReplacementParser());
+            BindingContext = new ScheduleViewModel(reportRepo);
+        }
+    }
+
+}
