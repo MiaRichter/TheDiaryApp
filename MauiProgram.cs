bd3d188c391efdfcf518e90e4eecb5a99ce136a0
Marci <152101518+MiaRichter@users.noreply.github.com>
2025-02-14 19:53:42 +0500

    Починил

---
+++ MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Toolkit.Hosting;
+
+namespace TheDiaryApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.ConfigureSyncfusionToolkit()
+			.ConfigureMauiHandlers(handlers =>
+			{
+			})
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+		builder.Services.AddLogging(configure => configure.AddDebug());
+#endif
+        builder.Services.AddSingleton<ReportRepo>();
+		builder.Services.AddSingleton<ExcelParser>();
+		builder.Services.AddSingleton<ReplacementParser>();
+
+		
+		return builder.Build();
+	}
+}
