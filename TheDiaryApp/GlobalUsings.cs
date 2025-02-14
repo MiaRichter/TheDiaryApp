2913b5f656e3290567fefcdebd8924a12172200a
Marci <152101518+MiaRichter@users.noreply.github.com>
2025-02-14 21:22:03 +0500

    Delete TheDiaryApp directory

--- TheDiaryApp/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using TheDiaryApp.Repositories;
-global using TheDiaryApp.Models;
-global using TheDiaryApp.Helpers;
-global using TheDiaryApp.ViewModels;
-global using Microsoft.Maui.Networking;
