88c2fab96056c972cc58a76ec55bc61c084114fa
Mia <Mia@DESKTOP-RQBIKKS>
2025-02-28 20:19:16 +0500

    Что то менял

--- TheDiaryApp/Handlers/CustomEntryHandler .cs
+++ TheDiaryApp/Handlers/CustomEntryHandler .cs
@@ -13,24 +13,5 @@ namespace TheDiaryApp.Handlers
 {
     public partial class CustomEntryHandler : EntryHandler
     {
-#if ANDROID
-        protected override void ConnectHandler(AppCompatEditText platformView)
-        {
-            base.ConnectHandler(platformView);
-
-            // Убираем встроенную границу на Android
-            platformView.Background = null;
-        }
-#endif
-
-#if IOS
-        protected override void ConnectHandler(UITextField platformView)
-        {
-            base.ConnectHandler(platformView);
-
-            // Убираем встроенную границу на iOS
-            platformView.BorderStyle = UITextBorderStyle.None;
-        }
-#endif
     }
 }
